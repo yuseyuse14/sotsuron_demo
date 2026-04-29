d1854e007a650240fa35f646c18647566b68744b
yuse <yu.ayamomik@gmail.com>
2026-04-29 13:44:31 +0900

    docs: アプリURLの追加

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 研究室見学用の卒論デモ
+学部1年生向けの簡易的な研究内容体験アプリです。
 
-学部1年生向けの簡易的な研究内容体験
+## アプリを試す
+| URL | QRコード |
+| :--- | :--- |
+| https://context-abbr.streamlit.app | <img src="./assets/app_qr.png" width="100"> |
--- qr.py
+++ qr.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import qrcode
 
 URL = "https://context-abbr.streamlit.app"
-OUT_DIR = Path("outputs")
+OUT_DIR = Path("./assets")
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 
 img = qrcode.make(URL)
-img.save(OUT_DIR / "qrcode.png")
+img.save(OUT_DIR / "app_qr.png")
